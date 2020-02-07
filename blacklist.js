28e1124742f088ae311b7e5118aa94aca7b076fd
chenYoon <33418054+cyjy1994@users.noreply.github.com>
2020-02-07 21:23:45 +0800

    Update and rename sdasd.js to blacklist.js

---
+++ blacklist.js
@@ -0,0 +1,12 @@
+//Customize blacklist
+let blacklist=['徐大虾咯','科技美学','敬汉卿','NathanRich火锅大王','千户长生']
+
+let body = $response.body
+body=JSON.parse(body)
+body['data']['items'].forEach((element, index)=> {
+   if(element.hasOwnProperty('ad_info')||element.hasOwnProperty('banner_item')||element['card_type']!='small_cover_v2'||blacklist.includes(element['args']['up_name'])){ 
+         body['data']['items'].splice(index,1)  
+    }
+})
+body=JSON.stringify(body)
+$done({body})
